8781e158db7814b727851adf9775384a58ce4588
Harssh Ssarma <harssh.ssarma@gmail.com>
2024-11-14 21:03:16 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
   try {
     // Log the MongoDB URI to ensure it's correctly loaded
-    console.log('MONGO_URI:', process.env.MONGO_URI);
+    console.log('MONGO_URL:', process.env.MONGO_URL);
 
     // Make sure MONGO_URI is a valid string
-    if (!process.env.MONGO_URI) {
-      throw new Error('MongoDB URI is missing from environment variables');
+    if (!process.env.MONGO_URL) {
+      throw new Error('MongoDB URL is missing from environment variables');
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useUnifiedTopology: true,
       useFindAndModify: false, // Optional: if you're using `findOneAndUpdate`
     });
